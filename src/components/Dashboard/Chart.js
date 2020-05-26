7eea987ddb835d04483e06c2ad35b46d88bfce32
Denys <maletsden@gmail.com>
2020-05-27 01:18:28 +0300

    feat: changed chart to show amount of rents by months

--- src/components/Dashboard/Chart.js
+++ src/components/Dashboard/Chart.js
@@ -1,23 +1,44 @@
 import React from 'react';
-import { useTheme } from '@material-ui/core/styles';
-import { LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer } from 'recharts';
+import {useTheme} from '@material-ui/core/styles';
+import {LineChart, Line, XAxis, YAxis, Label, ResponsiveContainer} from 'recharts';
 import Title from '../MainComponent/Title';
 
 // Generate Sales Data
-function createData(time, amount) {
-  return { time, amount };
+function createData(month, amount) {
+  return {month, amount};
 }
 
+// function getLast12Months() {
+//   const theMonths = [
+//     "January", "February", "March",
+//     "April", "May", "June", "July",
+//     "August", "September", "October",
+//     "November", "December"
+//   ];
+//   let last12Months = new Array(12);
+//   const today = new Date();
+//   let aMonth = today.getMonth();
+//
+//   for (let i = 0; i < 12; i++) {
+//     last12Months[11 - i] = theMonths[aMonth--];
+//     if (aMonth < 0) aMonth = 11;
+//   }
+//   return last12Months;
+// }
+
 const data = [
-  createData('00:00', 0),
-  createData('03:00', 300),
-  createData('06:00', 600),
-  createData('09:00', 800),
-  createData('12:00', 1500),
-  createData('15:00', 2000),
-  createData('18:00', 2400),
-  createData('21:00', 2400),
-  createData('24:00', undefined),
+  createData('January', 12),
+  createData('February', 231),
+  createData('March', 545),
+  createData('April', 332),
+  createData('May', 223),
+  createData('June', 627),
+  createData('July', 323),
+  createData('August', 233),
+  createData('September', 132),
+  createData('October', 232),
+  createData('November', 445),
+  createData('December', 123),
 ];
 
 export default function Chart() {
@@ -25,7 +46,7 @@ export default function Chart() {
 
   return (
     <React.Fragment>
-      <Title>Today</Title>
+      <Title>Total rents by last 12 months</Title>
       <ResponsiveContainer>
         <LineChart
           data={data}
@@ -36,17 +57,17 @@ export default function Chart() {
             left: 24,
           }}
         >
-          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
+          <XAxis dataKey="month" stroke={theme.palette.text.secondary}/>
           <YAxis stroke={theme.palette.text.secondary}>
             <Label
               angle={270}
               position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
+              style={{textAnchor: 'middle', fill: theme.palette.text.primary}}
             >
-              Sales ($)
+              Total rents
             </Label>
           </YAxis>
-          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false} />
+          <Line type="monotone" dataKey="amount" stroke={theme.palette.primary.main} dot={false}/>
         </LineChart>
       </ResponsiveContainer>
     </React.Fragment>
